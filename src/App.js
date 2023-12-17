e1c4529d770fcc6614d4604da77c4592e21caaac
Sara <prakasitadewi@gmail.com>
2023-12-17 16:16:32 +1100

    Installed bootstrap

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <a
-        href="https://github.com/prakadw-dev/react-final-app"
-        target="_blank"
-        rel="noreferrer"
-      >
-        Github Link
-      </a>
+      <div className="container">
+        <h1>Weather App</h1>
+        <a
+          href="https://github.com/prakadw-dev/react-final-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Github Link
+        </a>
+      </div>
     </div>
   );
 }
