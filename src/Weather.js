3e1bd011124220167548731793b7c34a42ce1e95
Sara <prakasitadewi@gmail.com>
2023-12-19 16:01:32 +1100

    Disable console.log

--- src/Weather.js
+++ src/Weather.js
@@ -7,7 +7,7 @@ export default function Weather() {
   const [weatherData, setWeatherData] = useState({ ready: false });
 
   function handleResponse(response) {
-    console.log(response.data);
+    // console.log(response.data);
     setWeatherData({
       ready: true,
       temperature: response.data.main.temp,
